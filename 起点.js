c0e81ae6f5ec49bc601c2f4311c5469cf6188996
CJunliang <1138768606@qq.com>
2020-12-22 14:11:43 +0800

    修复一些bug

--- 起点.js
+++ 起点.js
@@ -1,23 +1,30 @@
 auto.waitFor()
 // app名字和视频加载时长
 var appName = "起点读书"
-var loadTime = 5
+var loadTime = 8
+// 领取 立即领取 和 马上抢 红包的次数
+// var times1 = 0, times2 = 0;
+// 遍历的书籍本数
+var bookNum = 0;
 launchApp(appName);
 // 等待视频加载
 sleep(3000);
 
-var list = id("layoutRoot").find();
-var length = list.length;
+var length = id("layoutRoot").find().length;
 console.log("length:", length)
 
 for (var i = 0; i < length; i++) {
+    // 当遍历的书籍数量超过15本结束
+    if (bookNum >= 15)
+        break;
     var list = id("layoutRoot").find();
     var child = list[i];
     var book = child.findOne(id("coveLayout"));
+    bookNum++;
     // 书名
     var bookName = child.findOne(id("bookNameTxt"));
 
-    if (bookName) 
+    if (bookName)
         console.log(i, ':bookname: ', bookName.text())
     if (book) {
         // 点击书籍
@@ -30,6 +37,11 @@ for (var i = 0; i < length; i++) {
             back();
         }
         sleep(1000);
+    }
+    if (i == 5) {
+        length = list.length;
+        i = -1;
+        className("androidx.recyclerview.widget.RecyclerView").scrollForward();
     }
 }
 
@@ -46,14 +58,16 @@ function viewWebAD() {
     sleep(1000);
     // 红包
     // 点击红包按钮
-    id("layout_hongbao").findOne().click();
-    if (id("layoutHongbaoRoot").exists()) {
-        console.log("马上抢")
-        id("layoutHongbaoRoot").findOne().click();
-        sleep(Math.floor(Math.random() * 5 + 15 + loadTime) * 1000)
-        // 点击左上角退出并确认
-        className("android.widget.ImageView").findOne().click()
-        className("android.widget.TextView").text("关闭广告").click();
+    if (id("layout_hongbao").exists()) {
+        id("layout_hongbao").findOne().click();
+        if (id("layoutHongbaoRoot").exists()) {
+            console.log("马上抢")
+            id("layoutHongbaoRoot").findOne().click();
+            sleep(Math.floor(Math.random() * 5 + 15 + loadTime) * 1000)
+            // 点击左上角退出并确认
+            className("android.widget.ImageView").findOne().click()
+            className("android.widget.TextView").text("关闭广告").click();
+        }
     }
     sleep(1000);
     back()
